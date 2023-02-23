d7c9375693d93ce0cbd183e72acb0d81d0c6a15a
noorkobari <noor.kobari9@gmail.com>
2023-02-23 20:04:21 +0200

    no message

--- restaurantsManagement/src/app/services/table/table.service.ts
+++ restaurantsManagement/src/app/services/table/table.service.ts
@@ -11,45 +11,64 @@ export class TableService {
         {
         id:1,
         status:'Empty',
+        name:'Table 1',
       },
       {
         id:2,
         status:'Empty',
+        name:'Table 2',
+
       },
       {
         id:3,
         status:'Busy',
+        name:'Table 3',
+
       }
       ,
       {
         id:4,
         status:'Busy',
+        name:'Table 4',
+
       }
       ,
       {
         id:5,
         status:'Busy',
+        name:'Table 5',
+
       }
       ,
       {
         id:6,
         status:'Busy',
+        name:'Table 6',
+
       },
       {
         id:7,
         status:'Busy',
+        name:'Table 7',
+
       },
       {
         id:8,
         status:'Busy',
+        name:'Table 8',
+
       },
       {
         id:9,
         status:'Empty',
+        name:'Table 9',
+
       },
       {
         id:10,
         status:'Busy',
+        name:'Table 10',
+
       }
     ]
     }
