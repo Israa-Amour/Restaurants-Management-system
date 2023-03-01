238e9a092d8b5ca292e8a95a3df8cf7f4d8c2df4
noorkobari <noor.kobari9@gmail.com>
2023-03-01 12:27:16 +0200

    edit port

--- src/app/services/table/table.service.ts
+++ src/app/services/table/table.service.ts
@@ -2,29 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Table } from 'src/app/interfaces/table.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableService {
-
-  private apiUrl = 'http://localhost:8084';
-
   constructor(private http: HttpClient) { }
 
   getAll(){
-    return this.http.get<Table[]>(`${this.apiUrl}/tables`);
+    return this.http.get<Table[]>(`${environment.serverUrl}/tables`);
   }
 
   add(table: Table): Observable<Table> {
-    return this.http.post<Table>(`${this.apiUrl}/addTable`, table);
+    return this.http.post<Table>(`${environment.serverUrl}/addTable`, table);
   }
 
   update(table: Table): Observable<Table> {
-    return this.http.put<Table>(`${this.apiUrl}/updateTable/${table.id}`, table);
+    return this.http.put<Table>(`${environment.serverUrl}/updateTable/${table.id}`, table);
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/deleteTable/${id}`);
+    return this.http.delete<void>(`${environment.serverUrl}/deleteTable/${id}`);
   }
 }
