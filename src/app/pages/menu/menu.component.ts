3b10f60463f2768ed2bdf15a4f9d5feca6c1b9e2
noorkobari <noor.kobari9@gmail.com>
2023-03-03 04:37:44 +0200

    connect menu page with the backend

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -19,26 +19,20 @@ export class MenuComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Subscribe to changes in the route parameters
     this.route.paramMap.subscribe(params => {
       const categoryId = params?.get('categoryID');
       if (categoryId) {
-        // Get the items for the selected category
-        //we use the + to convert categoryId from a string to a number, cause the getItemsByCategoryId method parameter is a number.
         this.menuService.getItemsByCategoryId(+categoryId).subscribe(items => {
           this.items = items;
         });
       }
       else {
-        // Get all items
         this.menuService.getAll().subscribe(items => {
           this.items = items;
         });
       }
     });
     
-
-    // Get all categories
     this.menuService.getAllCategories().subscribe(categories => {
       this.categories = categories;
     });
